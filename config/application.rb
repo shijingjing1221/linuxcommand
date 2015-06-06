05bb3fcee33eeab80d2edd028e55545188c4b166
Shi Jingjing <jshi@redhat.com>
2015-06-06 13:17:10 +0800

    Have a basic UI

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ end
 
 module RailsApp
   class Application < Rails::Application
-    require "/project/command/config/initializers/bower_rails.rb"
+    # require "/project/command/config/initializers/bower_rails.rb"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,56 @@
-var mainApp = angular.module("mainApp", []);
-mainApp.controller("MainController",MainController);
-function MainController($scope){
-$scope.title = "Demo";
-}
+var mainApp = angular.module("mainApp", ['ui.select']);
+mainApp.controller("MainController", function($scope) {
+    $scope.title = "Search commands";
+    $scope.keyNames = [{
+        id: 1,
+        name: "NFS"
+    }, {
+        id: 2,
+        name: "iSCSI"
+    }];
+
+    // $scope.KeyNames = ProductsApi.query({
+    //     name_only: true
+    // });
+    $scope.keys = {};
+    $scope.keys.loaded = {};
+    $scope.removeKey = function(item) {
+        _.remove($scope.keys.wanted, function(n) {
+            return n.id === item.id;
+        });
+        // delete $scope.products.watned[item.id];
+        delete $scope.keys.loaded[item.id];
+
+    };
+
+    $scope.addKey = function(item) {
+        if (!$scope.keys.loaded[item.id]) {
+            $scope.loading = true;
+            $scope.keys.loaded[item.id] = {
+                name:item.name,
+                commands: [{
+                    name: "yum install nfs"
+                }, {
+                    name: "systemctl restart nfsserver"
+                }],
+                files: [{
+                    name: "/etc/nfsserver.conf"
+                }, {
+                    name: "/etc/exports"
+                }]
+            };
+            // ProductsApi.get({
+            //     id: item.id
+            // }).$promise.then(function(data) {
+            //     $scope.products.loaded[data.id] = data;
+            //     data.versions.forEach(function(version, index) {
+            //         if (version.eoldate > today) {
+            //             version.show = true;
+            //         }
+            //     });
+            //     $scope.loading = false;
+            // });
+        }
+    };
+});
+// angular.bootstrap(document, ['mainApp']);
