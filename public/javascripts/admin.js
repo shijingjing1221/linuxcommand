2f3434156e25e0774132d5aaf2fd91928065cd53
Shi Jingjing <jshi@redhat.com>
2015-07-03 20:13:32 +0800

    Add a admin page

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,21 +1,21 @@
-var mainApp = angular.module("mainApp", ['ui.select', 'ngResource', 'ngClipboard','ui.bootstrap']);
+var adminApp = angular.module("adminApp", ['ui.select', 'ngResource', 'ngClipboard', 'ui.bootstrap']);
 
 
-mainApp.factory("KeywordsApi", function($resource) {
+adminApp.factory("KeywordsApi", function($resource) {
     return $resource('/api' + '/keywords/:id', {
         id: '@id'
     }, {});
 });
 
-mainApp.controller("MainController", function($scope, KeywordsApi) {
-    $scope.title = "Command search ";
+adminApp.controller("AdminController", function($scope, KeywordsApi) {
+    $scope.title = "Select keyword to edit";
 
     $scope.keywordNames = KeywordsApi.query({
         name_only: true
     });
     $scope.keywords = {};
     $scope.keywords.loaded = {};
-    
+
     $scope.removeKeyword = function(item) {
         $scope.keywords.wanted = _.filter($scope.keywords.wanted, function(keyword) {
             return keyword.id != item.id;
@@ -36,6 +36,7 @@ mainApp.controller("MainController", function($scope, KeywordsApi) {
             });
         }
     };
+
 
 
 });
