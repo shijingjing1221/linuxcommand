b0dfe9891c4f767bf8d2489a59c9cda7e5440a34
Shi Jingjing <jshi@redhat.com>
2015-07-03 20:56:09 +0800

    Update the admin page to single select

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -8,34 +8,22 @@ adminApp.factory("KeywordsApi", function($resource) {
 });
 
 adminApp.controller("AdminController", function($scope, KeywordsApi) {
-    $scope.title = "Select keyword to edit";
+    $scope.title = "Select a keyword to edit";
 
     $scope.keywordNames = KeywordsApi.query({
         name_only: true
     });
-    $scope.keywords = {};
-    $scope.keywords.loaded = {};
+    $scope.keyword = {};
+    $scope.keyword.selected = undefined;
+    $scope.currentContent = undefined;
 
-    $scope.removeKeyword = function(item) {
-        $scope.keywords.wanted = _.filter($scope.keywords.wanted, function(keyword) {
-            return keyword.id != item.id;
+    $scope.$watch('keyword.selected', function(item) {
+        KeywordsApi.get({
+            id: item.id
+        }).$promise.then(function(data) {
+            $scope.currentContent = data;
         });
-        delete $scope.keywords.loaded[item.id];
-
-    };
-
-    $scope.addKeyword = function(item) {
-        if (!$scope.keywords.loaded[item.id]) {
-            $scope.loading = true;
-
-            KeywordsApi.get({
-                id: item.id
-            }).$promise.then(function(data) {
-                $scope.keywords.loaded[data.id] = data;
-                $scope.loading = false;
-            });
-        }
-    };
+    });
 
 
 
