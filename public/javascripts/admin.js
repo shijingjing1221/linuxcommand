9a632e4a29c8dcd5c5495b60fc438d867ed2bf20
Shi Jingjing <jshi@redhat.com>
2015-07-03 22:15:00 +0800

    Add new button to add configuration file(half done,just finish the UI, not store it back to server)

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -16,8 +16,11 @@ adminApp.controller("AdminController", function($scope, KeywordsApi) {
     $scope.keyword = {};
     $scope.keyword.selected = undefined;
     $scope.currentContent = undefined;
+    $scope.newfile = null;
 
     $scope.$watch('keyword.selected', function(item) {
+        if (item == null || item.id == null)
+            return;
         KeywordsApi.get({
             id: item.id
         }).$promise.then(function(data) {
@@ -25,6 +28,21 @@ adminApp.controller("AdminController", function($scope, KeywordsApi) {
         });
     });
 
+    $scope.addNewFile = function() {
+        $scope.currentContent.files.push({
+            id: null,
+            name: $scope.newfile
+        });
+        $scope.newfile = null;
+    };
+
+    $scope.addNewCommand = function() {
+        $scope.currentContent.commands.push({
+            id: null,
+            name: $scope.newCommand
+        });
+        $scope.newCommand = null;
+    };
 
 
 });
