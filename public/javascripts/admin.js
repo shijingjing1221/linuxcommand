e7af5b3c036493bce99ca28d86601f47cd25f4cf
Shi Jingjing <jshi@redhat.com>
2015-07-22 21:11:27 +0800

    Change the note to popover rather than the p.desc

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -29,6 +29,24 @@ adminApp.factory("ResourcesApi", function($resource) {
     });
 });
 
+adminApp.filter('unsafe', ['$sce', function ($sce) {
+    return function (val) {
+        return $sce.trustAsHtml(val);
+    };
+}]);
+// update popover template for binding unsafe html
+angular.module("template/popover/popover.html", []).run(["$templateCache", function ($templateCache) {
+    $templateCache.put("template/popover/popover.html",
+      "<div class=\"popover {{placement}}\" ng-class=\"{ in: isOpen(), fade: animation() }\">\n" +
+      "  <div class=\"arrow\"></div>\n" +
+      "\n" +
+      "  <div class=\"popover-inner\">\n" +
+      "      <h3 class=\"popover-title\" ng-bind-html=\"title | unsafe\" ng-show=\"title\"></h3>\n" +
+      "      <div class=\"popover-content\"ng-bind-html=\"content | unsafe\"></div>\n" +
+      "  </div>\n" +
+      "</div>\n" +
+      "");
+}]);
 
 adminApp.controller("AdminController", function($scope, KeywordsApi, ResourcesApi) {
     $scope.loadingContent = "Loading the keywords from server...";
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,6 +7,25 @@ mainApp.factory("KeywordsApi", function($resource) {
     }, {});
 });
 
+mainApp.filter('unsafe', ['$sce', function ($sce) {
+    return function (val) {
+        return $sce.trustAsHtml(val);
+    };
+}]);
+// update popover template for binding unsafe html
+angular.module("template/popover/popover.html", []).run(["$templateCache", function ($templateCache) {
+    $templateCache.put("template/popover/popover.html",
+      "<div class=\"popover {{placement}}\" ng-class=\"{ in: isOpen(), fade: animation() }\">\n" +
+      "  <div class=\"arrow\"></div>\n" +
+      "\n" +
+      "  <div class=\"popover-inner\">\n" +
+      "      <h3 class=\"popover-title\" ng-bind-html=\"title | unsafe\" ng-show=\"title\"></h3>\n" +
+      "      <div class=\"popover-content\"ng-bind-html=\"content | unsafe\"></div>\n" +
+      "  </div>\n" +
+      "</div>\n" +
+      "");
+}]);
+
 mainApp.controller("MainController", function($scope, KeywordsApi) {
     $scope.loadingContent = "Loading the keywords from server...";
     $scope.loading = true;
