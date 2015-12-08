2e34358a3d3c511080d4c719aa56e0cb0df2722c
Jingjing Shi <jshi@redhat.com>
2015-12-08 21:42:28 +0800

    Add the add modal instead of ayly input elments in admin page

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,7 +1,7 @@
 var adminApp = angular.module("adminApp", ['ui.select', 'ngResource', 'ngClipboard', 'ui.bootstrap', 'ui.bootstrap.modal']);
 
 
-adminApp.factory("KeywordsApi", function ($resource) {
+adminApp.factory("KeywordsApi", function($resource) {
     return $resource('/api' + '/keywords/:id', {
         id: '@id'
     }, {
@@ -14,7 +14,7 @@ adminApp.factory("KeywordsApi", function ($resource) {
     });
 });
 
-adminApp.factory("ResourcesApi", function ($resource) {
+adminApp.factory("ResourcesApi", function($resource) {
     return $resource('/api' + '/keywords/:id' + '/resources/:resourceType', {
         id: '@id',
         resourceType: '@resourceType'
@@ -29,7 +29,7 @@ adminApp.factory("ResourcesApi", function ($resource) {
     });
 });
 
-adminApp.factory("ResourceApi", function ($resource) {
+adminApp.factory("ResourceApi", function($resource) {
     return $resource('/api' + '/resource/:resourceId', {
         resourceId: '@resourceId'
     }, {
@@ -43,13 +43,13 @@ adminApp.factory("ResourceApi", function ($resource) {
     });
 });
 
-adminApp.filter('unsafe', ['$sce', function ($sce) {
-    return function (val) {
+adminApp.filter('unsafe', ['$sce', function($sce) {
+    return function(val) {
         return $sce.trustAsHtml(val);
     };
 }]);
 // update popover template for binding unsafe html
-angular.module("template/popover/popover.html", []).run(["$templateCache", function ($templateCache) {
+angular.module("template/popover/popover.html", []).run(["$templateCache", function($templateCache) {
     $templateCache.put("template/popover/popover.html",
         "<div class=\"popover {{placement}}\" ng-class=\"{ in: isOpen(), fade: animation() }\">\n" +
         "  <div class=\"arrow\"></div>\n" +
@@ -62,7 +62,7 @@ angular.module("template/popover/popover.html", []).run(["$templateCache", funct
         "");
 }]);
 
-adminApp.controller("AdminController", function ($scope, $modal, $log, KeywordsApi, ResourcesApi, ResourceApi) {
+adminApp.controller("AdminController", function($scope, $modal, $log, KeywordsApi, ResourcesApi, ResourceApi) {
     $scope.loadingContent = "Loading the keywords from server...";
     $scope.loading = true;
     $scope.title = "Linux Command";
@@ -73,7 +73,7 @@ adminApp.controller("AdminController", function ($scope, $modal, $log, KeywordsA
 
     $scope.keywordNames = KeywordsApi.query({
         name_only: true
-    }, function (data) {
+    }, function(data) {
         $scope.loading = false;
     });
     $scope.keyword = {};
@@ -81,24 +81,22 @@ adminApp.controller("AdminController", function ($scope, $modal, $log, KeywordsA
     $scope.currentContent = undefined;
     $scope.newFile = null;
 
-    $scope.$watch('keyword.selected', function (item) {
+    $scope.$watch('keyword.selected', function(item) {
         if (item == null || item.id == null)
             return;
         KeywordsApi.get({
             id: item.id
-        }).$promise.then(function (data) {
+        }).$promise.then(function(data) {
             $scope.currentContent = data;
         });
     });
 
-    $scope.addNewFile = function () {
+    $scope.addNewFile = function(newItem) {
         var files = $scope.currentContent.files;
-        var newFile = $scope.newFile;
-        var newFileNote = $scope.newFileNote;
+        var newFile = newItem.name;
+        var newFileNote = newItem.note;
         var result = _.find(files, _.matchesProperty('name', newFile));
         if (result != undefined) {
-            $scope.newFile = null;
-            $scope.newFileNote = null;
             return;
         }
         files.push({
@@ -106,26 +104,22 @@ adminApp.controller("AdminController", function ($scope, $modal, $log, KeywordsA
             name: newFile,
             note: newFileNote
         });
-        $scope.newFile = null;
-        $scope.newFileNote = null;
         ResourcesApi.CreateNewResource({
             id: $scope.keyword.selected.id,
             resourceType: "file",
             name: newFile,
             note: newFileNote
-        }).$promise.then(function (data) {
-            $scope.currentContent = data;
+        }).$promise.then(function(data) {
+            // $scope.currentContent = data;
         });
     };
 
-    $scope.addNewCommand = function () {
+    $scope.addNewCommand = function(newItem) {
         var commands = $scope.currentContent.commands;
-        var newCommand = $scope.newCommand;
-        var newCommandNote = $scope.newCommandNote;
+        var newCommand = newItem.name;
+        var newCommandNote = newItem.note;
         var result = _.find(commands, _.matchesProperty('name', newCommand));
         if (result != undefined) {
-            $scope.newCommand = null;
-            $scope.newCommandNote = null;
             return;
         }
         commands.push({
@@ -133,20 +127,19 @@ adminApp.controller("AdminController", function ($scope, $modal, $log, KeywordsA
             name: newCommand,
             note: newCommandNote
         });
-        $scope.newCommand = null;
-        $scope.newCommandNote = null;
+
         ResourcesApi.CreateNewResource({
             id: $scope.keyword.selected.id,
             resourceType: "command",
             name: newCommand,
             note: newCommandNote
-        }).$promise.then(function (data) {
+        }).$promise.then(function(data) {
             $scope.currentContent = data;
         });
 
     };
 
-    $scope.addNewKeyword = function () {
+    $scope.addNewKeyword = function() {
         var newKeyword = $scope.newKeyword;
         var keywords = $scope.keywordNames;
         var result = _.find(keywords, _.matchesProperty('name', newKeyword));
@@ -165,50 +158,91 @@ adminApp.controller("AdminController", function ($scope, $modal, $log, KeywordsA
 
         KeywordsApi.CreateNewKeyword({
             name: newKeyword
-        }).$promise.then(function (data) {
+        }).$promise.then(function(data) {
             keywordObj.id = data.id;
             $scope.currentContent = data;
         });
     };
-    $scope.openEditor = function (selectedItem) {
+    $scope.openEditor = function(selectedItem) {
 
         var modalInstance = $modal.open({
             animation: $scope.animationsEnabled,
-            templateUrl: 'myModalContent.html',
-            controller: 'ModalInstanceCtrl',
+            templateUrl: 'EditorModalContent.html',
+            controller: 'EditorModalInstanceCtrl',
             resolve: {
-                selectedItem: function () {
+                selectedItem: function() {
                     return selectedItem;
                 }
             }
         });
 
-        modalInstance.result.then(function (item) {
+        modalInstance.result.then(function(item) {
             $scope.item = item;
             ResourceApi.UpdateResource({
                 resourceId: item.id,
                 name: item.name,
                 note: item.note
-            }).$promise.then(function (data) {
+            }).$promise.then(function(data) {
                 $scope.item.name = item.name;
                 $scope.item.note = item.note;
             })
-        }, function () {
+        }, function() {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+
+    $scope.openCreator = function(type) {
+
+        var modalInstance = $modal.open({
+            animation: $scope.animationsEnabled,
+            templateUrl: 'CreatorModalContent.html',
+            controller: 'CreatorModalInstanceCtrl',
+            resolve: {
+                type: function() {
+                    return type;
+                }
+            }
+        });
+
+        modalInstance.result.then(function(item) {
+            $scope.item = item;
+            if (type == 'command') {
+                $scope.addNewCommand(item);
+            } else {
+                $scope.addNewFile(item);
+            }
+        }, function() {
             $log.info('Modal dismissed at: ' + new Date());
         });
     };
 
 });
 
-adminApp.controller('ModalInstanceCtrl', function ($scope, $modalInstance, selectedItem) {
+adminApp.controller('EditorModalInstanceCtrl', function($scope, $modalInstance, selectedItem) {
 
     $scope.item = selectedItem;
 
-    $scope.ok = function () {
+    $scope.ok = function() {
         $modalInstance.close($scope.item);
     };
 
-    $scope.cancel = function () {
+    $scope.cancel = function() {
+        $modalInstance.dismiss('cancel');
+    };
+});
+
+
+adminApp.controller('CreatorModalInstanceCtrl', function($scope, $modalInstance, type) {
+
+    $scope.item = {
+        "rtype": type === "command" ? 0 : 1
+    };
+
+    $scope.ok = function() {
+        $modalInstance.close($scope.item);
+    };
+
+    $scope.cancel = function() {
         $modalInstance.dismiss('cancel');
     };
 });
