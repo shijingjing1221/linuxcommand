77ab7ebca0b894c567f5b06215d8c2f9b528bc31
Jingjing Shi <jshi@redhat.com>
2015-12-18 20:41:54 +0800

    add tags and source{title, link}

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,4 +1,25 @@
 class Resource < ActiveRecord::Base
-  attr_accessible :name, :rtype, :note
-  has_many :mappers
+    attr_accessible :name, :rtype, :note
+    has_many :mappers
+
+
+    def tags
+        tags = []
+        for m in self.mappers do
+            tags << m.keyword.name
+            return tags
+        end
+    end
+
+    def source
+        return  {title:  "Linux command", link: "http://www.linuxcommand.cn"}
+    end
+
+    def get_defined_json
+        self.to_json(
+        {
+            methods: [:tags,:source],
+            only:[:id, :name, :rtype]
+        })
+    end
 end
