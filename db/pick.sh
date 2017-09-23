8003dfd50d536312d1b3052843461527eaf033ae
Jingjing <shijingjing1221@gmail.com>
2017-09-23 20:30:29 +0800

    add tow file

--- db/pick.sh
+++ db/pick.sh
@@ -15,6 +15,15 @@ remove_commands=(
     "cat"
     "(gdb) "
     "(while"
+    "lvcreate"
+    "targetcli"
+    "iscsiadm"
+    "mkfs"
+    "nmcli"
+    "hostnamectl"
+    "INSERT"
+    "systemctl"
+    "yum install"
     )
 
 for command in ${remove_commands[@]};do
@@ -22,8 +31,17 @@ for command in ${remove_commands[@]};do
     sed -i "/^${command}/ d" command_resources_name_picked.txt 
 done
 
+remove_contain_strings=(
+    "classroom"
+    
+    )
+
+for string in ${remove_contain_strings[@]};do
+    echo "${command}";
+    sed -i "/${string}/ d" command_resources_name_picked.txt 
+done
 
 sed -i "s/^sudo //g" command_resources_name_picked.txt
 
 awk '!seen[$0]++' command_resources_name_picked.txt > command_resources_name_picked_unq.txt
-sed -n '/-/p' command_resources_name_picked_unq.txt > command_resources_name_picked_unq_matchoption.txt
+sed -n '/ -/p' command_resources_name_picked_unq.txt > command_resources_name_picked_unq_matchoption.txt
